5aa5e628079cd79e661f0babf09f277832edd1a7
tokkaya79 <tokkaya79@gmail.com>
2025-02-04 16:01:16 +0100

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+const IndexPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-background">
+      <h1 className="text-4xl font-normal text-green-500">Hello, Next.js with Tailwind and SCSS! And typscript too!!!</h1>
+    </div>
+  );
+};
+
+export default IndexPage;
